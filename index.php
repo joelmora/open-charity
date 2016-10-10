97d2ee492236d454455e0b71e420ae670eb6446c
Joel Mora <joelmora16@gmail.com>
2016-10-09 21:57:00 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,286 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <script src="js/jquery-1.9.1.min.js"></script>
+    <script src="js/owl-carousel/owl.carousel.js"></script>
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/grid.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/responsive.css">
+    <link rel="stylesheet" href="css/font-awesome.css">
+    <link rel="stylesheet" href="js/owl-carousel/owl.carousel.css">
+    <link rel="stylesheet" href="js/owl-carousel/owl.theme.css">
+    <title>Joel Mora - Open Charity</title>
+</head>
+<body>
+<div id="container">
+
+    <div class="top-bar section">
+        <div class="row">
+            <div class="logo-container col-sm-12 col-md-4">
+                <a href="#" title="Open Charity" rel="home" id="logo">
+                    <img src="images/logo.png" alt="Logo" />
+                </a>
+            </div>
+            <div class="menu-container col-sm-12 col-md-8">
+                <div class="menu-item">
+                    <a href="#">About Open Charity</a>
+                </div>
+                <div class="menu-item">
+                    <a href="#">The blog</a>
+                </div>
+                <div class="menu-action">
+                    <a href="#">
+                        <button>Join Us</button>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!--Hero-->
+    <div class="hero section">
+        <div class="hero-title">
+            Sharing ideas for charities
+        </div>
+        <div class="hero-subtitle hidden-xs">
+            Many charities' goals are similar, as is the functionality we require, but little
+            shared working takes place.
+        </div>
+        <div class="hero-subtitle small hidden-xs">
+            By working together, driving shared areas of interest and influencing open source<br>
+            developments we can bring efficiencies, improve the digital experience for our users, and
+            have great impact.
+        </div>
+        <div class="hero-subtitle hidden-xs">
+            Together we can make a bigger difference.
+        </div>
+    </div>
+
+    <!--Events-->
+    <div class="events section">
+        <div class="event-container">
+
+            <div class="row">
+                <div class="col-xs-12 col-sm-10">
+                    <div class="event-title">
+                        <span class="highlight">Next Event:</span>
+                        <span>June 23<sup>rd</sup> 2016 <span class="hidden-xs">18:30 - 21:00</span></span>
+                    </div>
+                    <div class="event-description hidden-xs">
+                        Cancer Research UK, Angel Building, 407 St John Street, London EC1V 4AD
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-2">
+                    <div class="event-action">
+                        <a href="#">
+                            <button>Register</button>
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <!--Involved-->
+    <div class="involved section">
+
+        <div class="row">
+            <div class="col-xs-12 title">
+                Get Involved
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xs-12 col-sm-4">
+                <div class="group">
+                    <div class="group-image">
+                        <img src="images/meet-up.png"/>
+                    </div>
+                    <div class="group-title">We Do Meetings</div>
+                    <div class="group-content hidden-xs">We organise our meetings through the OpenCharity MeetUp group</div>
+                    <div class="group-action">
+                        <a href="#">
+                            <button>Meetup Group</button>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-4">
+                <div class="group">
+                    <div class="group-image">
+                        <img src="images/slack.png"/>
+                    </div>
+                    <div class="group-title">We Collaborate</div>
+                    <div class="group-content hidden-xs">OpenCharity have a slack group for daily collaboration</div>
+                    <div class="group-action">
+                        <a href="#">
+                            <button>Slack Group</button>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-4">
+                <div class="group">
+                    <div class="group-image">
+                        <img src="images/share.png"/>
+                    </div>
+                    <div class="group-title">We Share</div>
+                    <div class="group-content hidden-xs">We have a Google Group set up to share tools and documents</div>
+                    <div class="group-action">
+                        <a href="#">
+                            <button>Google Group</button>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+    <!--Mission-->
+    <div class="mission section">
+
+        <div class="row">
+            <div class="col-xs-12 title">
+                our mission
+            </div>
+            <div class="col-xs-12 subtitle small hidden-xs">
+                Charities and Partners collaborating and sharing open solutions and ideas to create value in the digital space.
+            </div>
+            <div class="col-xs-12 subtitle hidden-xs">
+                If you are a charity or a supplier, we are ready to welcome you.
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xs-12 col-sm-4">
+                <div class="group">
+                    <div class="group-image">
+                        <img src="images/bulb.png"/>
+                    </div>
+                    <div class="group-background">
+                        <div class="group-title">We help charities</div>
+                        <div class="group-content">Share knowledge and working practice to make the best technology choices.</div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-4">
+                <div class="group">
+                    <div class="group-image">
+                        <img src="images/together.png"/>
+                    </div>
+                    <div class="group-background">
+                        <div class="group-title">We bring together</div>
+                        <div class="group-content">Charities and suppliers to the charity sector to share best practices.</div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-4">
+                <div class="group">
+                    <div class="group-image">
+                        <img src="images/thumbs.png"/>
+                    </div>
+                    <div class="group-background">
+                        <div class="group-title">We encourage</div>
+                        <div class="group-content">Collaboration and innovation for the good of the charity sector.</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+    <div class="section separator">
+        <hr>
+    </div>
+
+    <!--Members-->
+    <div class="members section">
+
+        <div class="row">
+            <div class="col-xs-12 title">
+                our members
+            </div>
+        </div>
+        <div id="carousel-members" class="owl-carousel">
+            <?php for ($i = 0; $i < 3; $i++):?>
+                <div>
+                    <img class="img-cancer-research" src="images/cancer-research.png"/>
+                </div>
+                <div>
+                    <img class="img-compucorp" src="images/compucorp.png"/>
+                </div>
+                <div>
+                    <img class="img-kop" src="images/kop.png"/>
+                </div>
+                <div>
+                    <img class="img-zing" src="images/zing.png"/>
+                </div>
+                <div>
+                    <img class="img-comic-relief" src="images/comic-relief.png"/>
+                </div>
+            <?php endfor; ?>
+        </div>
+    </div>
+
+    <!--Blog-->
+    <div class="blog section">
+
+        <div class="row">
+            <div class="col-xs-12 title">
+                blog
+            </div>
+        </div>
+        <div id="blog-entries-carousel">
+            <?php for ($i = 0; $i < 14; $i++):?>
+                <div class="blog-entry">
+                    <div class="blog-entry-title">Online Donations Special...</div>
+                    <div class="blog-entry-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </div>
+                    <div class="blog-entry-footer">14 Nov 2014</div>
+                </div>
+            <?php endfor; ?>
+        </div>
+
+    </div>
+
+
+    <!--Footer-->
+    <div class="footer section">
+
+        <div class="social-icon-container">
+            <i class="fa fa-facebook"></i>
+            <i class="fa fa-twitter"></i>
+            <i class="fa fa-google-plus"></i>
+        </div>
+
+        <hr>
+
+        <div class="copyright">
+            <div>
+                This site was built as a collaboration between
+                <a class="highlight" href="#">Manifesto Digital</a> and
+                <a class="highlight" href="#">Compucorp</a>
+            </div>
+        </div>
+
+    </div>
+</div>
+<script>
+    jQuery(document).ready(function($) {
+
+        $("#carousel-members").owlCarousel({
+            autoPlay: true
+        });
+        $("#blog-entries-carousel").owlCarousel({
+            items: 4,
+            navigation: true,
+            navigationText: ['<i class="fa fa-chevron-left"></i>','<i class="fa fa-chevron-right"></i>'],
+            pagination: false,
+            autoPlay: true
+        });
+
+    });
+</script>
+</body>
+</html>
